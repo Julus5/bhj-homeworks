9e6950fc11af2b640c0a361a37ef9990fa29436c
Julus5 <matveev.dulustaan@mail.ru>
2023-08-14 03:43:13 +0700

    Задание 3 выполнено

---
+++ html-forms/interests/task.js
@@ -0,0 +1,25 @@
+let interests = document.querySelectorAll(".interests");
+
+for (let item of interests) {
+    const closest = item.closest(".interest");
+    if (closest) {
+        const checkBox = closest.querySelector(".interest__check");
+        checkBox.onclick = () => {
+            if (checkBox.checked) {
+                interestCheck(closest, true);
+            } else {
+                interestCheck(closest, false);
+            };
+        };
+    };
+};
+
+function interestCheck(element, checked) {
+    let items = element.querySelectorAll(".interest__check");
+    for (let i of items) {
+        i.checked = checked;
+    };
+};
+
+
+
